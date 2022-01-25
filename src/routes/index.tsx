9a1ed5b1b8b895d9c75b592cfa2c258f6aef4906
Marcelo-MRS <marcelo_mr.s@hotmail.com>
2022-01-25 12:38:45 -0300

    Config navigation structure

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Home} from '../pages';
+
+type StackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
+
+const Routes: React.FC = () => (
+  <Stack.Navigator initialRouteName="Home">
+    <Stack.Screen name="Home" component={Home} />
+  </Stack.Navigator>
+);
+
+export default Routes;
